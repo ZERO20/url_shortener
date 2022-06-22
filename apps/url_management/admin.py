c367b2a3972f2290aa1adeb25be84dd019a1420d
Edgar de la Cruz <edgar@miflink.com>
2022-06-22 11:02:00 -0500

    feat: added model and admin class

--- apps/url_management/admin.py
+++ apps/url_management/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from apps.url_management.models import Shortener
 # Register your models here.
+
+
+@admin.register(Shortener)
+class ShortenerAdmin(admin.ModelAdmin):
+    fields = ('url', 'shortcode', 'created_at', 'updated_at',)
+    readonly_fields = ('created_at', 'updated_at',)
+    list_display = ('url', 'shortcode', 'created_at',)
+    list_filter = ('created_at',)
+    search_fields = ('url', 'short_code')
--- apps/url_management/models.py
+++ apps/url_management/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Shortener(models.Model):
+    url = models.URLField(unique=True)
+    shortcode = models.URLField(unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["-created_at"]
+        verbose_name = 'Shortener'
+        verbose_name_plural = 'Shorteners'
+
+    def __str__(self):
+        return f'{self.url} to {self.shortcode}'
